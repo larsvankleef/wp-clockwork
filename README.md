413d66ed78cc2f440f7c2e8ed4649c9b030744ab
larsvankleef <larsvankleef@outlook.com>
2021-02-23 11:13:24 +0100

    First draft

---
+++ README.md
@@ -0,0 +1,10 @@
+{
+  "type": "path",
+  "url": "/Users/lvankleef/Code/Kaliber/kaliber-higgins-elasticsearch-connector",
+  "options": {
+    "symlink": true,
+    "version": {
+      "higgins/higgins-elasticsearch-connector": "2.0.1-dev"
+    }
+  }
+}
---
+++ wp-clockwork.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Plugin Name: Clockwork for WordPress
+ * Description: WordPress plugin for Clockwork.
+ * Author: Lars van Kleef
+ * Version: 1.0.0
+ * Author URI: https://larsvankleef.nl
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+class WpClockWork {
+  private $clockwork;
+  private $clockwork_start;
+  private $clockwork_end;
+
+  function __construct() {
+    $this->clockwork = Clockwork\Support\Vanilla\Clockwork::init([ 'register_helpers' => true ]);
+
+    add_action('init', [$this, 'init']);
+    add_action('shutdown', [$this, 'shutdown']);
+    add_action('parse_request', [$this, 'parse_request']);
+  }
+
+  function init() {
+    define('SAVEQUERIES', true);
+    
+    $this->clockwork_start = microtime(true);
+    header("X-Clockwork-Id: " . $this->clockwork->getRequest()->id);
+    header("X-Clockwork-Version: " . '5.0.6');
+  
+    $this->clockwork->addDataSource(new Clockwork\DataSource\PhpDataSource());
+    $this->clockwork->notice('Application Started');
+  }
+  
+  function shutdown() {
+    global $wpdb;
+
+    $request = $this->clockwork->getRequest();
+    $queries = array_map(function ($query) {
+      log_to_file('query', $query);
+      return ['query' => $query[0], 'duration' => $query[1] ];
+    }, $wpdb->queries ?: []);
+
+    $request->databaseQueries = $queries;
+    $this->clockwork->notice('Shutdown');
+    $this->clockwork->requestProcessed();
+  }
+
+  function parse_request() {
+    $uri = $_SERVER['REQUEST_URI'];
+
+    if (preg_match('/\/__clockwork\/.*/', $uri)) {
+      $request = explode('/', $uri);
+      $storage = new Clockwork\Storage\FileStorage(__DIR__."/clockwork/");
+      $data = $storage->find($request[2]);
+
+      echo $data->toJson();
+      exit();
+    }
+  }
+}
+
+new WpClockWork();
